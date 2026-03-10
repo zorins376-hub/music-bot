2bce2f14d4d8cc14b25cae9f2362f5826609637c
sherhan1988hp <134908489+33hpS@users.noreply.github.com>
2026-03-11 03:23:16 +0600

    fix: uniform bold font for TEQUILA SUNSHINE subtitle

--- webapp/frontend/src/App.tsx
+++ webapp/frontend/src/App.tsx
@@ -575,7 +575,7 @@ export function App() {
             marginTop: 3,
             letterSpacing: 2,
             textTransform: "uppercase",
-          }}>inspired by T𝗘𝗤𝗨𝗜𝗟𝗔 𝗦𝗨𝗡𝗦𝗛𝗜𝗡𝗘.</div>
+          }}>inspired by 𝗧𝗘𝗤𝗨𝗜𝗟𝗔 𝗦𝗨𝗡𝗦𝗛𝗜𝗡𝗘.</div>
         </div>
       )}
 
